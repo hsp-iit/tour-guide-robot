b94fb59d937e2b01bef3a9677a73ff819b2f4dcf
DatSpace <konkarapas@outlook.com>
2022-06-13 16:53:48 +0000

    Added conf

--- app/navigation2/launch/amcl_sim.launch.py
+++ app/navigation2/launch/amcl_sim.launch.py
@@ -5,22 +5,21 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     amcl_params = os.path.join(os.getcwd(), 'amcl_params.yaml')
-    # par2 = [{'use_sim_time': True},
-    #         {'autostart': True},
-    #         {'node_names': ['amcl']}]
+    par = [{'use_sim_time': True},
+           {'autostart': True},
+           {'node_names': ['amcl']}]
     return LaunchDescription([
         Node(
             package='nav2_amcl',
             executable='amcl',
             name='amcl',
             parameters=[amcl_params]
-        )])
-    # ),
-    # Node(
-    #     package='nav2_lifecycle_manager',
-    #     executable='lifecycle_manager',
-    #     name='lifecycle_manager_localization',
-    #     output='screen',
-    #     parameters= par2
-    # )
-    # ])
+        ),
+        Node(
+            package='nav2_lifecycle_manager',
+            executable='lifecycle_manager',
+            name='lifecycle_manager_localization',
+            output='screen',
+            parameters=par
+        )
+    ])
