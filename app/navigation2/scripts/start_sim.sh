a0a3bd7ac4608a164edf289871cce68d0e969db5
DatSpace <konkarapas@outlook.com>
2022-06-14 18:24:42 +0200

    Updated xmls

--- app/navigation2/scripts/start_sim.sh
+++ app/navigation2/scripts/start_sim.sh
@@ -1,6 +1,6 @@
 yarpserver &
 sleep 1;
-gazebo /home/user1/tour-guide-robot/app/maps/SIM_GAM/GAM.world &
+ros2 launch gazebo_ros gazebo.launch.py world:=/home/user1/tour-guide-robot/app/maps/SIM_GAM/GAM.world &
 sleep 1;
 yarprun --server /console --log &
 sleep 1;
