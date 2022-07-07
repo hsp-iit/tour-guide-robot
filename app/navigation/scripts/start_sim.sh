b4b6cbadfe40b7e288761d65867381e2847bd666
Stefano Bernagozzi <stefano.bernagozzi@iit.it>
2022-07-07 16:42:28 +0200

    added gazebo

--- app/navigation/scripts/start_sim.sh
+++ app/navigation/scripts/start_sim.sh
@@ -2,6 +2,10 @@ roscore &
 sleep 2;
 yarpserver --ros &
 sleep 1;
-rosrun gazebo_ros gzserver -s libgazebo_yarp_clock.so /home/user1/tour-guide-robot/app/maps/SIM_GAM/GAM.world &
+rosrun gazebo_ros gazebo -s libgazebo_yarp_clock.so /home/user1/tour-guide-robot/app/maps/SIM_GAM/GAM.world &
 sleep 2;
 yarprun --server /console --log &
+sleep 1;
+yarplogger --start &
+sleep 1;
+yarpmanager
