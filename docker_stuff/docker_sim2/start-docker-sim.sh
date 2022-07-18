29face77f73d51c9945d00065e2936b1a900df9c
DatSpace <konkarapas@outlook.com>
2022-07-18 18:40:24 +0200

    Added configuration for ros2 on robot and docker r1Core2

--- docker_stuff/docker_sim2/start-docker-sim.sh
+++ docker_stuff/docker_sim2/start-docker-sim.sh
@@ -1,7 +1,7 @@
 sudo xhost +
 FILE=~/.bashrc_local
 if [ -f "$FILE" ]; then
-  sudo docker run --rm -it --privileged --network host --pid host -e NVIDIA_DRIVER_CAPABILITIES=all -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v ~/.bashrc_local:/home/user1/.bashrc_local -v /etc/hosts:/etc/hosts -e QT_X11_NO_MITSHM=1 --gpus all konkarapas/r1images:r1Sim2
+  sudo docker run --rm -it --privileged --network host --pid host -e NVIDIA_DRIVER_CAPABILITIES=all -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /etc/hosts:/etc/hosts -e QT_X11_NO_MITSHM=1 --gpus all konkarapas/r1images:r1Sim2
 else
-  sudo docker run --rm -it --privileged --network host --pid host -e NVIDIA_DRIVER_CAPABILITIES=all -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v ${PWD}/.bashrc_local:/home/user1/.bashrc_local -e QT_X11_NO_MITSHM=1 --gpus all konkarapas/r1images:r1Sim2
+  sudo docker run --rm -it --privileged --network host --pid host -e NVIDIA_DRIVER_CAPABILITIES=all -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -e QT_X11_NO_MITSHM=1 --gpus all konkarapas/r1images:r1Sim2
 fi
