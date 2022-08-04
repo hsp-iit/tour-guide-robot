00a0e0fc97114fc0d1bedaa31ce91f13239039e2
DatSpace <konkarapas@outlook.com>
2022-08-04 11:39:49 +0200

    Clearing/Organising for ROS2

--- app/navigation/scripts/demo_speech.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "setLanguage \"en-US\" \"en-US-Wavenet-C\"" | yarp rpc /googleSynthesis/rpc
-echo "say \"Hello everyone. Should we start the demo?\"" | yarp rpc /googleSynthesis/rpc
