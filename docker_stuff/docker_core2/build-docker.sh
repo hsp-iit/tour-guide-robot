101aa4106d438693bca6ce6160a15e82a0e3e84e
Ettore Landini <ettore.landini@iit.it>
2022-10-28 10:55:14 +0200

    Options for new openpose 1.7 image
    start-docker-core.sh and build-docker.sh modified in order to be able to launch and build also the openpose1p7 image
    
    Added also a new Dockerfile for the openpose1p7 image

--- docker_stuff/docker_core2/build-docker.sh
+++ docker_stuff/docker_core2/build-docker.sh
@@ -3,6 +3,8 @@
 if [[ -n $1 ]]; then
     if [[ $1 == "-d"  || $1 == "--devel" ]]; then
         sudo docker build -t elandini84/r1images:r1Core2_devel .
+    elif [[ $1 == "-o"  || $1 == "--openpose" ]]; then
+        sudo docker build -f Dockerfile_openpose1p7 -t elandini84/r1images:r1Core2_devel_openpose1p7 .
     elif [[ $1 == "-s"  || $1 == "--stable" ]]; then
         sudo docker build -t elandini84/r1images:r1Core2_stable .
     elif [[ $1 == "-h"  || $1 == "--help" ]]; then
@@ -10,6 +12,7 @@ if [[ -n $1 ]]; then
         echo "options:"
         echo "    -d, --devel     Build the image with the r1Core2_devel tag"
         echo "    -s, --stable    Build the image with the r1Core2_stable tag"
+        echo "    -o, --openpose  Build the image with the r1Core2_devel_openpose1p7 tag and using the docker file Dockerfile_openpose1p7"
         echo "    -h, --help      See current help"
         echo "If no option is passed, the r1Core2_devel tag is used"
     else
--- docker_stuff/docker_core2/start-docker-core.sh
+++ docker_stuff/docker_core2/start-docker-core.sh
@@ -1,33 +1,27 @@
 #!/bin/bash
 
 sudo xhost +
-FILE=~/.bashrc_local
 
 if [[ -n $1 ]]; then
     if [[ $1 == "-d"  || $1 == "--devel" ]]; then
-        if [ -f "$FILE" ]; then
-            sudo docker run --rm -it --privileged --network host --pid host -e NVIDIA_DRIVER_CAPABILITIES=all -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /etc/hosts:/etc/hosts -e QT_X11_NO_MITSHM=1 --gpus all elandini84/r1images:r1Core2_devel
-        else
-            sudo docker run --rm -it --privileged --network host --pid host -e NVIDIA_DRIVER_CAPABILITIES=all -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -e QT_X11_NO_MITSHM=1 --gpus all elandini84/r1images:r1Core2_devel
-        fi
+        sudo docker run --rm -it --privileged --network host --pid host -e NVIDIA_DRIVER_CAPABILITIES=all -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /etc/hosts:/etc/hosts -e QT_X11_NO_MITSHM=1 --gpus all elandini84/r1images:r1Core2_devel
     elif [[ $1 == "-s"  || $1 == "--stable" ]]; then
-        if [ -f "$FILE" ]; then
-            sudo docker run --rm -it --privileged --network host --pid host -e NVIDIA_DRIVER_CAPABILITIES=all -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /etc/hosts:/etc/hosts -e QT_X11_NO_MITSHM=1 --gpus all elandini84/r1images:r1Core2_stable
-        else
-            sudo docker run --rm -it --privileged --network host --pid host -e NVIDIA_DRIVER_CAPABILITIES=all -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -e QT_X11_NO_MITSHM=1 --gpus all elandini84/r1images:r1Core2_stable
-        fi
+        sudo docker run --rm -it --privileged --network host --pid host -e NVIDIA_DRIVER_CAPABILITIES=all -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /etc/hosts:/etc/hosts -e QT_X11_NO_MITSHM=1 --gpus all elandini84/r1images:r1Core2_stable
+    elif [[ $1 == "-o"  || $1 == "--openpose" ]]; then
+        sudo docker run --rm -it --privileged --network host --pid host -e NVIDIA_DRIVER_CAPABILITIES=all -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /etc/hosts:/etc/hosts -e QT_X11_NO_MITSHM=1 --gpus all elandini84/r1images:r1Core2_devel_openpose1p7
     elif [[ $1 == "-h"  || $1 == "--help" ]]; then
         echo "Syntax: ./start-docker-core [option]"
         echo "options:"
         echo "    -d, --devel     Starts the image with the r1Core2_devel tag"
+        echo "    -o, --openpose  Starts the image with the r1Core2_devel_openpose1p7 tag"
         echo "    -s, --stable    Starts the image with the r1Core2_stable tag"
         echo "    -h, --help      See current help"
-        echo "If no option is passed, the r1Core2_devel tag is used"
+        echo "If no option is passed, the r1Core2_stable tag is used"
     else
         echo "Wrong option."
         $0 -h
     fi
 else
 	echo "No option passed. Using the default tag"
-    $0 -d
+    $0 -s
 fi
