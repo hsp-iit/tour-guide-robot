882403d37c7744550c90922dd6458085b5222976
Ettore Landini <ettore.landini@iit.it>
2022-11-30 11:55:43 +0100

    tourCore2 imge now from nvidia docker
    Now the tourCore2 image does not derive from the standard ubuntu22.04
    image but from nvidia/cuda:11.7.1-cudnn8-devel-ubuntu22.04. A new docker
    file has been added in order to be able to compile the old style core
    image (from ubuntu:22.04)

--- docker_stuff/docker_tourCore2/build-docker.sh
+++ docker_stuff/docker_tourCore2/build-docker.sh
@@ -5,11 +5,14 @@ if [[ -n $1 ]]; then
         sudo docker build -t elandini84/r1images:tourCore2_devel .
     elif [[ $1 == "-s"  || $1 == "--stable" ]]; then
         sudo docker build -t elandini84/r1images:tourCore2_stable .
+    elif [[$1 == "-n" || $1 == "--no-nvidia"]]; then
+        sudo docker build -t -f DockerfileNoNvidia elandini84/r1images:tourCore2_noNvidia .
     elif [[ $1 == "-h"  || $1 == "--help" ]]; then
         echo "Syntax: ./build-docker.sh [option]"
         echo "options:"
         echo "    -d, --devel     Build the image with the tourCore2_devel tag"
         echo "    -s, --stable    Build the image with the tourCore2_stable tag"
+        echo "    -n, --no-nvidia Build the image starting from ubuntu standard and not the nvidia image"
         echo "    -h, --help      See current help"
         echo "If no option is passed, the tourCore2_devel tag is used"
     else
