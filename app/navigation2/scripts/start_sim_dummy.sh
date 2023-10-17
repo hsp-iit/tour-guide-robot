5910517b9c05bee57de6c56d5d0bc395dc5fb53e
Raffaele Colombo <colombraf@gmail.com>
2023-10-17 12:40:29 +0200

    added dummy.world

--- app/navigation2/scripts/start_sim_dummy.sh
+++ app/navigation2/scripts/start_sim_dummy.sh
@@ -1,6 +1,6 @@
 yarpserver &
 sleep 1;
-ros2 launch gazebo_ros gazebo.launch.py extra_gazebo_args:=-slibgazebo_yarp_clock.so &
+ros2 launch gazebo_ros gazebo.launch.py extra_gazebo_args:=-slibgazebo_yarp_clock.so world:=/home/user1/tour-guide-robot/app/maps/SIM_GAM/dummy.world &
 sleep 1;
 yarprun --server /console --log &
 sleep 1;
