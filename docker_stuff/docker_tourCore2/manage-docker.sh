91800969418776d3732d037685e2e0f0d16bef1a
Ettore Landini <ettore.landini@iit.it>
2024-01-08 14:30:25 +0100

    Yarp dependencies fixed - Yarp branch as Dockerfile arg
    Following the imminent deprecation of the old  http://www.icub.org/ubuntu repository the yarp are now explicitly installed during docker images building. Also, it's now possible to set the yarp branch to use while building the aforementioned images

--- docker_stuff/docker_tourCore2/manage-docker.sh
+++ docker_stuff/docker_tourCore2/manage-docker.sh
@@ -15,6 +15,8 @@ usage()
     echo "    -d, --devel                        Build/run the image with the '$DEVEL_SUFFIX' tag"
     echo "    -e, --repo  + \"repo_name\"          Build/run the image with the passed repository reference"
     echo "    -r, --ros_distro + \"distro_name\"   Build/run the image with the passed distro (the passed value will be also used to compose the image tag)"
+    echo "    -y, --yarp_branch + \"yarp branch\"  Build/run the image with the passed yarp branch (the passed value, if different from \"master\", will be also used to compose the image tag)."
+    echo "                                       If not passed, the branch used will be \"master\""
     echo "    -h, --help                         See current help"
     echo "If the parent image is not specified (neither -u nor -c), the '$UBUNTU_DEF' one will be used"
     echo "If the build type is not specified (neither -d nor -s), the '$DEVEL_SUFFIX' tag will be used"
@@ -86,6 +88,16 @@ get_opts()
                 ROS_SET=true
                 shift
                 ;;
+            -y|--yarp_vers)
+                if [[ $YARP_SET == "true" ]]; then
+                    echo "YARP branch already set already set"
+                    usage
+                fi
+                shift
+                YARP_BRANCH=$1
+                YARP_SET=true
+                shift
+                ;;
             -e|--repository)
                 if [[ $REPO_SET == "true" ]]; then
                     echo "Remote repository already set"
@@ -132,12 +144,15 @@ BASE_TAG_DEF="tourCore2"
 BUILD_SUFFIX=$DEVEL_SUFFIX
 VERSION="1.0.0"
 ROS_DISTRO=$ROS_DEF
+YARP_BRANCH=$YARP_DEF
+YARP_TAG=""
 PARENT_SUFFIX=$UBUNTU_SUFFIX
 IMAGE_SET=false
 BUILD_SET=false
 GONNA_BUILD=false
 RUN_WITH_GPU=true
 ROS_SET=false
+YARP_SET=false
 IMAGE=$UBUNTU_DEF
 REPO=$REPO_DEF
 REPO_SET=false
@@ -149,10 +164,13 @@ BASE_TAG=$BASE_TAG_DEF
 # Get the options
 get_opts $@
 
-COMPLETE_IMAGE_NAME=$REPO$REPO_SEP$BASE_TAG$JUNCTION$PARENT_SUFFIX$JUNCTION$ROS_DISTRO$JUNCTION$BUILD_SUFFIX
+if [[ $YARP_BRANCH != "master" ]]; then
+    YARP_TAG=$YARP_BRANCH$JUNCTION
+fi
+COMPLETE_IMAGE_NAME=$REPO$REPO_SEP$BASE_TAG$JUNCTION$PARENT_SUFFIX$JUNCTION$ROS_DISTRO$JUNCTION$YARP_TAG$BUILD_SUFFIX
 
 if [[ $GONNA_BUILD == "true" ]]; then
-    sudo docker build --build-arg base_img=$IMAGE --build-arg ros_distro=$ROS_DISTRO -t $COMPLETE_IMAGE_NAME .
+    sudo docker build --build-arg base_img=$IMAGE --build-arg ros_distro=$ROS_DISTRO --build-arg yarp_branch=$YARP_BRANCH  -t $COMPLETE_IMAGE_NAME .
 else
     sudo xhost +
     if [[ $RUN_WITH_GPU == "true" ]]; then
