99c53b76354f355f5078dab6c33ca545ca80af1a
Ettore Landini <ettore.landini@iit.it>
2024-02-21 14:18:40 +0100

    Navigation2 app done. TBT

--- app/navigation2/scripts/start_sim.sh
+++ app/navigation2/scripts/start_sim.sh
@@ -1,6 +1,6 @@
 yarpserver &
 sleep 1;
-ros2 launch gazebo_ros gazebo.launch.py extra_gazebo_args:=-slibgazebo_yarp_clock.so world:=/home/user1/tour-guide-robot/app/maps/SIM_GAM/GAM.world &
+ros2 launch gazebo_ros gazebo.launch.py extra_gazebo_args:=-slibgazebo_yarp_clock.so world:=${TOUR_GUIDE_ROBOT_SOURCE_DIR}/app/maps/SIM_GAM/GAM.world &
 sleep 1;
 yarprun --server /console --log &
 sleep 1;
