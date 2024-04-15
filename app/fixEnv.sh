f7ed31ffebe678a5a23e8607136cd549f638c503
Ettore Landini <ettore.landini@iit.it>
2024-04-15 16:53:10 +0200

    Added PATH setting to fixEnv

--- app/fixEnv.sh
+++ app/fixEnv.sh
@@ -1,7 +1,9 @@
 if [ -z "${TOUR_GUIDE_ROBOT_SOURCE_DIR}" ]; then
     if [ -z "${HSP_CODE}" ]; then
+        echo "Setting TOUR_GUIDE_ROBOT_SOURCE_DIR to ~/tour-guide-robot"
         echo "export TOUR_GUIDE_ROBOT_SOURCE_DIR=~/tour-guide-robot" >> ~/.bashrc
     else
+        echo "Setting TOUR_GUIDE_ROBOT_SOURCE_DIR to ${HSP_CODE}/tour-guide-robot"
         echo "export TOUR_GUIDE_ROBOT_SOURCE_DIR=${HSP_CODE}/tour-guide-robot" >> ~/.bash_profile
     fi
 else
@@ -10,10 +12,18 @@ fi
 
 if [ -z "${CER_SIM_ROOT_DIR}" ]; then
     if [ -z "${ROBOT_CODE}" ]; then
+        echo "Setting CER_SIM_ROOT_DIR to ~/robotology/cer-sim"
         echo "export CER_SIM_ROOT_DIR=~/robotology/cer-sim" >> ~/.bashrc
     else
-        echo "export CER_SIM_ROOT_DIR=${ROBOT_CODE}/cer-sim" >> ~/.bash_profile
+        echo "Setting CER_SIM_ROOT_DIR to ${ROBOT_CODE}/cer-sim"
+        echo "export CER_SIM_ROOT_DIR=${ROBOT_CODE}/cer-sim" >> ~/.bashrc
     fi
 else
     echo "The env is already set to: ${CER_SIM_ROOT_DIR}"
+fi
+if [[ ":$PATH:" == *":app/headSynchronizer/scripts:"* ]]; then
+  echo "Your path is correctly set"
+else
+  echo "Your path is missing the path to the joypad scripts. Adding them."
+  echo "export PATH=\$PATH:${TOUR_GUIDE_ROBOT_SOURCE_DIR}/app/navigation2/scripts:${TOUR_GUIDE_ROBOT_SOURCE_DIR}/app/headSynchronizer/scripts" >> ~/.bashrc
 fi
