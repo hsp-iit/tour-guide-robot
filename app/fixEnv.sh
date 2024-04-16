ac3b91a715a59d13f6be3fa10dbf65c02de7559e
Ettore Landini <ettore.landini@iit.it>
2024-04-16 10:38:20 +0200

    fixEnv should be complete now

--- app/fixEnv.sh
+++ app/fixEnv.sh
@@ -1,7 +1,42 @@
+if [ -z "${CER_SIM_ROOT_DIR}" ]; then
+    if [ -z "${ROBOT_CODE}" ]; then
+        if [ -d "~/robotology/cer-sim" ]; then
+            echo "Setting CER_SIM_ROOT_DIR to ~/robotology/cer-sim"
+            echo "export CER_SIM_ROOT_DIR=~/robotology/cer-sim" >> ~/.bashrc
+        else
+            echo "Cer-sim directory not found"
+            echo "If you have cer-sim cloned on your machine, please set the CER_SIM_ROOT_DIR manually."
+            echo "Otherwise you can clone it by running the following command:"
+            echo "git clone https://github.com/robotology/cer-sim.git in a directory of your choice"
+        fi
+    else if [ -d "${ROBOT_CODE}/cer-sim" ]; then
+        echo "Setting CER_SIM_ROOT_DIR to ${ROBOT_CODE}/cer-sim"
+        echo "export CER_SIM_ROOT_DIR=${ROBOT_CODE}/cer-sim" >> ~/.bashrc
+    else
+        echo "Cer-sim directory not found"
+        echo "If you have cer-sim cloned on your machine, please set the CER_SIM_ROOT_DIR manually."
+        echo "Otherwise you can clone it by running the following command:"
+        echo "git clone https://github.com/robotology/cer-sim.git in a directory of your choice and try again"
+    fi
+else
+    echo "The env is already set to: ${CER_SIM_ROOT_DIR}"
+fi
+
 if [ -z "${TOUR_GUIDE_ROBOT_SOURCE_DIR}" ]; then
     if [ -z "${HSP_CODE}" ]; then
-        echo "Setting TOUR_GUIDE_ROBOT_SOURCE_DIR to ~/tour-guide-robot"
-        echo "export TOUR_GUIDE_ROBOT_SOURCE_DIR=~/tour-guide-robot" >> ~/.bashrc
+        if [ -z "${ROBOT_CODE}" ]; then
+            if [ -d "${ROBOT_CODE}/tour-guide-robot" ]; then
+                echo "Setting TOUR_GUIDE_ROBOT_SOURCE_DIR to ${ROBOT_CODE}/tour-guide-robot"
+                echo "export TOUR_GUIDE_ROBOT_SOURCE_DIR=${ROBOT_CODE}/tour-guide-robot" >> ~/.bashrc
+            else if [ -d "~/robotology/tour-guide-robot" ]; then
+                echo "Setting TOUR_GUIDE_ROBOT_SOURCE_DIR to ~/robotology/tour-guide-robot"
+                echo "export TOUR_GUIDE_ROBOT_SOURCE_DIR=~/robotology/tour-guide-robot" >> ~/.bashrc
+            else
+                echo "Tour-guide-robot directory not found"
+                echo "If you have tour-guide-robot cloned on your machine, please set the TOUR_GUIDE_ROBOT_SOURCE_DIR manually."
+                echo "Otherwise you can clone it by running the following command:"
+                echo "git clone https://github.com/hsp-iit/tour-guide-robot.git in a directory of your choice and try again"
+            fi
     else
         echo "Setting TOUR_GUIDE_ROBOT_SOURCE_DIR to ${HSP_CODE}/tour-guide-robot"
         echo "export TOUR_GUIDE_ROBOT_SOURCE_DIR=${HSP_CODE}/tour-guide-robot" >> ~/.bash_profile
@@ -10,17 +45,6 @@ else
     echo "The env is already set to: ${TOUR_GUIDE_ROBOT_SOURCE_DIR}"
 fi
 
-if [ -z "${CER_SIM_ROOT_DIR}" ]; then
-    if [ -z "${ROBOT_CODE}" ]; then
-        echo "Setting CER_SIM_ROOT_DIR to ~/robotology/cer-sim"
-        echo "export CER_SIM_ROOT_DIR=~/robotology/cer-sim" >> ~/.bashrc
-    else
-        echo "Setting CER_SIM_ROOT_DIR to ${ROBOT_CODE}/cer-sim"
-        echo "export CER_SIM_ROOT_DIR=${ROBOT_CODE}/cer-sim" >> ~/.bashrc
-    fi
-else
-    echo "The env is already set to: ${CER_SIM_ROOT_DIR}"
-fi
 if [[ ":$PATH:" == *":app/headSynchronizer/scripts:"* ]]; then
   echo "Your path is correctly set"
 else
