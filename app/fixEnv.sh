125aa71392b0be337fee0ea93b953a21ee8ea88d
Ettore Landini <ettore.landini@iit.it>
2024-04-18 09:52:17 +0200

    Added a simple test bench app for google speech modules

--- app/fixEnv.sh
+++ app/fixEnv.sh
@@ -25,12 +25,19 @@ fi
 if [ -z "${TOUR_GUIDE_ROBOT_SOURCE_DIR}" ]; then
     if [ -z "${HSP_CODE}" ]; then
         if [ -z "${ROBOT_CODE}" ]; then
+            if [ -d "~/robotology/tour-guide-robot" ]; then
+                echo "Setting TOUR_GUIDE_ROBOT_SOURCE_DIR to ~/robotology/tour-guide-robot"
+                echo "export TOUR_GUIDE_ROBOT_SOURCE_DIR=~/robotology/tour-guide-robot" >> ~/.bashrc
+            else
+                echo "Tour-guide-robot directory not found"
+                echo "If you have tour-guide-robot cloned on your machine, please set the TOUR_GUIDE_ROBOT_SOURCE_DIR manually."
+                echo "Otherwise you can clone it by running the following command:"
+                echo "git clone https://github.com/hsp-iit/tour-guide-robot.git in a directory of your choice and try again"
+            fi
+        else
             if [ -d "${ROBOT_CODE}/tour-guide-robot" ]; then
                 echo "Setting TOUR_GUIDE_ROBOT_SOURCE_DIR to ${ROBOT_CODE}/tour-guide-robot"
                 echo "export TOUR_GUIDE_ROBOT_SOURCE_DIR=${ROBOT_CODE}/tour-guide-robot" >> ~/.bashrc
-            elif [ -d "~/robotology/tour-guide-robot" ]; then
-                echo "Setting TOUR_GUIDE_ROBOT_SOURCE_DIR to ~/robotology/tour-guide-robot"
-                echo "export TOUR_GUIDE_ROBOT_SOURCE_DIR=~/robotology/tour-guide-robot" >> ~/.bashrc
             else
                 echo "Tour-guide-robot directory not found"
                 echo "If you have tour-guide-robot cloned on your machine, please set the TOUR_GUIDE_ROBOT_SOURCE_DIR manually."
