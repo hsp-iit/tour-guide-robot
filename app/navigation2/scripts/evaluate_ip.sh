9a3db6368381c6bf7b8301bc6abd3afc9a2d795c
fbrand-new <francesco.brand@iit.it>
2024-05-17 12:02:12 +0200

    Fix evaluate ip for cyclone_dds settings given current Erzelli configuration

--- app/navigation2/scripts/evaluate_ip.sh
+++ app/navigation2/scripts/evaluate_ip.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
+
+# Change: we want this to work with both 192.168.100 and 192.168.101
 current_ip=$(ifconfig | grep 192.168.10 | awk '{print $2}')
+
 echo $current_ip
-if [ $current_ip = "192.168.100.10" ];
-then
-  echo "base"
-  sed -i -e "s/REMOTEIP/192\.168\.100\.1/g" ${TOUR_GUIDE_ROBOT_SOURCE_DIR}/app/navigation2/conf/cyclone_dds_settings.xml
-else
-  echo "different"
-  sed -i -e "s/REMOTEIP/192\.168\.100\.10/g" ${TOUR_GUIDE_ROBOT_SOURCE_DIR}/app/navigation2/conf/cyclone_dds_settings.xml
-fi
+
+case "$current_ip" in
+  192.168.101.10)
+    echo "base"
+    sed -i -e "s/REMOTEIP/192\.168\.101\.1/g" ${TOUR_GUIDE_ROBOT_SOURCE_DIR}/app/navigation2/conf/cyclone_dds_settings.xml
+    ;;
+  192.168.100.10)
+    echo "base"
+    sed -i -e "s/REMOTEIP/192\.168\.100\.1/g" ${TOUR_GUIDE_ROBOT_SOURCE_DIR}/app/navigation2/conf/cyclone_dds_settings.xml
+    ;;
+  192.168.101.*)
+    echo "base"
+    sed -i -e "s/REMOTEIP/192\.168\.101\.10/g" ${TOUR_GUIDE_ROBOT_SOURCE_DIR}/app/navigation2/conf/cyclone_dds_settings.xml
+    ;;
+  192.168.100.*)
+    echo "base"
+    sed -i -e "s/REMOTEIP/192\.168\.100\.10/g" ${TOUR_GUIDE_ROBOT_SOURCE_DIR}/app/navigation2/conf/cyclone_dds_settings.xml
+    ;;
+  *)
+    echo "Unknown ip address"
+    ;;
+esac
+
 sed -i -e "s/LOCALIP/$current_ip/g" ${TOUR_GUIDE_ROBOT_SOURCE_DIR}/app/navigation2/conf/cyclone_dds_settings.xml
 
