b5a94f89b3ca9c8fa8284cc9ef4960a12e0c1028
Ettore Landini <ettore.landini@iit.it>
2024-05-29 19:17:14 +0200

    Entrypoint fixes
    Signed-off-by: Ettore Landini <ettore.landini@iit.it>

--- docker_stuff/docker_sim2/manage-docker.sh
+++ docker_stuff/docker_sim2/manage-docker.sh
@@ -176,7 +176,7 @@ else
     sudo xhost +
     if [[ $RUN_WITH_GPU == "true" ]]; then
         sudo docker run --rm -it --privileged --network host --pid host -e NVIDIA_DRIVER_CAPABILITIES=all -e DISPLAY -e ROS_DOMAIN_ID=${ROS_DOMAIN_ID} -v /tmp/.X11-unix:/tmp/.X11-unix -e QT_X11_NO_MITSHM=1 --gpus all $COMPLETE_IMAGE_NAME
-    elif [[ $RUN_WITH_GPU == "false" && $IMAGE == $UBUNTU_DEF ]]; then
+    elif [[ $RUN_WITH_GPU == "false" ]]; then
         sudo docker run --rm -it --privileged --network host --pid host -e DISPLAY -e ROS_DOMAIN_ID=${ROS_DOMAIN_ID} -v /tmp/.X11-unix:/tmp/.X11-unix -e QT_X11_NO_MITSHM=1 $COMPLETE_IMAGE_NAME
     else
         echo "ERROR: You cannot run a nVidia based image without gpu support"
