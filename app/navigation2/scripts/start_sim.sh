c3c9699d1c8e07951732e47a7ee8b148948cc7e9
Leonardo Gori <leonardo.gori@iit.it>
2025-04-07 13:27:23 +0000

    Added application for Robot Valley 2025

--- app/navigation2/scripts/start_sim.sh
+++ app/navigation2/scripts/start_sim.sh
@@ -1,4 +1,4 @@
-yarpserver &
+yarpserver --read &
 sleep 1;
 ros2 launch gazebo_ros gazebo.launch.py extra_gazebo_args:=-slibgazebo_yarp_clock.so world:=${TOUR_GUIDE_ROBOT_SOURCE_DIR}/app/maps/SIM_GAM/GAM.world &
 sleep 1;
