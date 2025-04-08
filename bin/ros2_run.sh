8413d99608c633a92185bd9f470a0e27900bbd6b
Ettore Landini <ettore.landini@iit.it>
2025-04-08 16:31:40 +0200

    Added ros2 run equivalent of ros2_launch.sh

---
+++ bin/ros2_run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# This scripts wraps ros2 launch handling the SIGTERM signal - currently bugged in ros2 (https://github.com/ros2/launch/issues/666)
+# In particular, the user can substitute the ros2 launch command in yarpmanager with this script.
+# The syntax is the same but the full path to the launch_file is needed.
+# Stopping the script via yarpmanager will correctly stop the nodes. Killing the script will leave the nodes dangling.
+
+
+_term() {
+	echo "Cleaning up the process"
+	kill -2 -${group}
+}
+
+if [ "$#" -lt 1 ]
+then
+	echo "Usage ros2_run.sh [package] <executable> [args]"
+	exit -1
+fi
+
+trap _term SIGTERM SIGINT
+
+setsid ros2 run $@ &
+
+group=$!
+echo "Group pid: $group"
+child=$!
+wait "$child"
