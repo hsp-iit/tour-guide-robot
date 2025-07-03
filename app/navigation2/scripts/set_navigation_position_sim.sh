0c1f8defe1217f3185cb43d7098f8e2289c305ea
Leonardo Gori <leonardo.gori@iit.it>
2025-07-03 09:55:22 +0000

    Added square map and config files for new r1mk3 model

--- app/navigation2/scripts/set_navigation_position_sim.sh
+++ app/navigation2/scripts/set_navigation_position_sim.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
-echo "set pos 0 -9.0" | yarp write ... /SIM_CER_ROBOT/right_arm/rpc:i
-echo "set pos 1 9.0" | yarp write ... /SIM_CER_ROBOT/right_arm/rpc:i
-echo "set pos 2 -10.0" | yarp write ... /SIM_CER_ROBOT/right_arm/rpc:i
-echo "set pos 3 50.0" | yarp write ... /SIM_CER_ROBOT/right_arm/rpc:i
-echo "set pos 4 0.0" | yarp write ... /SIM_CER_ROBOT/right_arm/rpc:i
-echo "set pos 5 0.0" | yarp write ... /SIM_CER_ROBOT/right_arm/rpc:i
-echo "set pos 6 0.0" | yarp write ... /SIM_CER_ROBOT/right_arm/rpc:i
-echo "set pos 7 0.0" | yarp write ... /SIM_CER_ROBOT/right_arm/rpc:i
+echo "set pos 0 16.0511" | yarp write ... /r1mk3Sim/right_arm/rpc:i
+echo "set pos 1 10.00549318" | yarp write ... /r1mk3Sim/right_arm/rpc:i
+echo "set pos 2 14.2878" | yarp write ... /r1mk3Sim/right_arm/rpc:i
+echo "set pos 3 61.5511" | yarp write ... /r1mk3Sim/right_arm/rpc:i
+echo "set pos 4 0.0109864" | yarp write ... /r1mk3Sim/right_arm/rpc:i
+echo "set pos 5 0.0329591" | yarp write ... /r1mk3Sim/right_arm/rpc:i
+echo "set pos 6 0.0" | yarp write ... /r1mk3Sim/right_arm/rpc:i
+echo "set pos 7 0.0" | yarp write ... /r1mk3Sim/right_arm/rpc:i
 
-echo "set pos 0 -9.0" | yarp write ... /SIM_CER_ROBOT/left_arm/rpc:i
-echo "set pos 1 9.0" | yarp write ... /SIM_CER_ROBOT/left_arm/rpc:i
-echo "set pos 2 -10.0" | yarp write ... /SIM_CER_ROBOT/left_arm/rpc:i
-echo "set pos 3 50.0" | yarp write ... /SIM_CER_ROBOT/left_arm/rpc:i
-echo "set pos 4 0.0" | yarp write ... /SIM_CER_ROBOT/left_arm/rpc:i
-echo "set pos 5 0.0" | yarp write ... /SIM_CER_ROBOT/left_arm/rpc:i
-echo "set pos 6 0.0" | yarp write ... /SIM_CER_ROBOT/left_arm/rpc:i
-echo "set pos 7 0.0" | yarp write ... /SIM_CER_ROBOT/left_arm/rpc:i
+echo "set pos 0 16.0511" | yarp write ... /r1mk3Sim/left_arm/rpc:i
+echo "set pos 1 10.00549318" | yarp write ... /r1mk3Sim/left_arm/rpc:i
+echo "set pos 2 14.2878" | yarp write ... /r1mk3Sim/left_arm/rpc:i
+echo "set pos 3 61.5511" | yarp write ... /r1mk3Sim/left_arm/rpc:i
+echo "set pos 4 0.0109864" | yarp write ... /r1mk3Sim/left_arm/rpc:i
+echo "set pos 5 0.0329591" | yarp write ... /r1mk3Sim/left_arm/rpc:i
+echo "set pos 6 0.0" | yarp write ... /r1mk3Sim/left_arm/rpc:i
+echo "set pos 7 0.0" | yarp write ... /r1mk3Sim/left_arm/rpc:i
 
-echo "set pos 0 0.01" | yarp write ... /SIM_CER_ROBOT/torso/rpc:i
+echo "set pos 0 0.012" | yarp write ... /r1mk3Sim/torso/rpc:i
+
