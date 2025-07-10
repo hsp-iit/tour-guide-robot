e5e8ba792fc9e269108fe9111663fe62f0912770
Ettore Landini <ettore.landini@iit.it>
2025-07-10 09:59:57 +0200

    docker-compose.yml for docker tal fixed

--- docker_stuff/docker_talk/entrypoints/entrypoint_light.sh
+++ docker_stuff/docker_talk/entrypoints/entrypoint_light.sh
@@ -7,7 +7,7 @@ tourBranch=$TOUR_BRANCH
 ROBOT_CODE=/usr/local/src/robot
 
 cd ${ROBOT_CODE}/tour-guide-robot/ && rm -rf build && mkdir build && \
-    (git fetch ${tourRemote} || (git remote add ${tourRemote} https://github.com/${tourRemote}/tour-guide-robot && git fetch ${tourRemote})) && git checkout ${tourBranch} && git pull && cd build && cmake .. && make -j11
+    (git fetch ${tourRemote} || (git remote add ${tourRemote} https://github.com/${tourRemote}/tour-guide-robot && git fetch ${tourRemote})) && git checkout ${tourBranch} && git pull && cd build && cmake .. -DENABLE_faceExpressionImage=ON -DSILERO_VAD=ON && make -j11
 cd ${ROBOT_CODE}/r1-object-retrieval/ && rm -rf build && mkdir build && \
     (git fetch ${r1ObrRemote} || (git remote add ${r1ObrRemote} https://github.com/${r1ObrRemote}/r1-object-retrieval && git fetch ${r1ObrRemote})) && git checkout ${r1ObrBranch} && git pull && cd build && cmake .. && make -j11
 exec "$@"
