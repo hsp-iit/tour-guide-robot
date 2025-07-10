352528b210598da9046cd8b91d2116b581e4efb9
Ettore Landini <ettore.landini@iit.it>
2025-07-10 09:41:34 +0200

    Added new entry point and fixed r1OBR CMakeLists

--- docker_stuff/docker_talk/entrypoints/env.sh
+++ docker_stuff/docker_talk/entrypoints/env.sh
@@ -2,6 +2,6 @@ export R1_OBR_REMOTE=elandini84
 export R1_OBR_BRANCH=test/sim_lobby
 export TOUR_REMOTE=origin
 export TOUR_BRANCH=feature/openWakeWord/exclude
-export ENTRY_POINT=/home/user1/config/.entrypoint_light.sh
+export ENTRY_POINT=/home/yarp-user/config/.entrypoint_light.sh
 
 xhost + local:docker
