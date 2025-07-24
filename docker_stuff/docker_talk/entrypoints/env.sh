74354e89ef938d4b90a6b71fa6705e5b86414a5b
Ettore Landini <ettore.landini@iit.it>
2025-07-24 16:30:24 +0200

    Updated tour-guide-robot branch in env.sh file

--- docker_stuff/docker_talk/entrypoints/env.sh
+++ docker_stuff/docker_talk/entrypoints/env.sh
@@ -1,7 +1,7 @@
 export R1_OBR_REMOTE=elandini84
 export R1_OBR_BRANCH=test/sim_lobby
 export TOUR_REMOTE=origin
-export TOUR_BRANCH=feature/openWakeWord/exclude
+export TOUR_BRANCH=jazzy
 export ENTRY_POINT=/home/yarp-user/config/.entrypoint_light.sh
 
 xhost + local:docker
