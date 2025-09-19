11677acb030d7c581f5d261a9de7375d4f4368c0
Leonardo Gori <leonardo.gori@iit.it>
2025-09-19 17:18:03 +0200

    added description file and workflow to automatically  generate readme

--- app/convince/doc_generator.py
+++ app/convince/doc_generator.py
@@ -1,17 +1,39 @@
+import json
+import os
 import xml.etree.ElementTree as ET
 from pathlib import Path
 
-def generate_table(xml_folder, md_file):
-    with open(md_file, "w") as f:
-            f.write(f"# Documentation\n\n")
+def generate_table(xml_folder):
+    # Path to the folder where the script lives
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    readme_path = os.path.join(script_dir, "README.md")
+
+    # read json file with module descriptions
+    json_file = Path(__file__).parent / "modules_description.json"
+    if json_file.exists():
+        with open(json_file) as f:
+            json_data = json.load(f)
+    else:
+        json_data = {}
+
+    projects_desc = json_data.get("projects", {})
+    applications_desc = json_data.get("applications", {})
+    modules_desc = json_data.get("modules", {})
+
+    with open(readme_path, "w") as f:
+            f.write(f"# Documentation\n")
+            f.write(f"{projects_desc.get('convince', '')}\n\n")
+
     for xml_file in Path(xml_folder).glob("*.xml"):
         # print(xml_file)
         tree = ET.parse(xml_file)
         root = tree.getroot()
 
         rows = []
-        with open(md_file, "a") as f:
-            f.write(f"## Documentation for {xml_file.stem}\n\n")
+        with open(readme_path, "a") as f:
+            f.write(f"## Description for {xml_file.stem}\n")
+            if applications_desc and xml_file.stem in applications_desc:
+                f.write(f"{applications_desc[xml_file.stem]}\n\n")
 
         for module in root.findall("module"):
             name = module.find("name").text.strip()
@@ -26,12 +48,22 @@ def generate_table(xml_folder, md_file):
             if node is not None:
                 desc.append(f"{node.text.strip()}")
 
-            rows.append(f"| `{name}` | {desc[0]} | {desc[1]} |")
+            # add description from json data if available
+            key = " ".join([name] + [desc[0]] if desc[0] else [name])
+            # print(f"Looking for key: {key}.")
+            if key in modules_desc:
+                desc.append(modules_desc[key])
+            else:
+                desc.append("")
 
-        with open(md_file, "a") as f:
-            f.write("| Module | Parameters  | Node |\n")
-            f.write("|--------|-------------|------|\n")
+            rows.append(f"| `{name}` | {desc[0]} | {desc[1]} | {desc[2]} |")
+
+        with open(readme_path, "a") as f:
+            f.write("| Module | Parameters  | Node | Description |\n")
+            # f.write("|--------|-------------|------|-------------|\n")
+            f.write("| :- | :------ | :- | :----------- |\n")
             f.write("\n".join(rows))
+            f.write("\n\n")
 
 if __name__ == "__main__":
-    generate_table("scripts", "README2.md")
+    generate_table("scripts")
