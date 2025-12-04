63670f3f5890275abcf51b4f17a6a1fccf630dcc
Ettore Landini <ettore.landini@iit.it>
2025-12-04 16:55:53 +0100

    Removed unnecessary gazebo package installation
    The gazebo version installed using the standalone package has been removed and with that the related options for the manage-docker script

--- docker_stuff/docker_sim/manage-docker.sh
+++ docker_stuff/docker_sim/manage-docker.sh
@@ -15,7 +15,6 @@ usage()
     echo "    -d, --devel                        Build/run the image with the '$DEVEL_SUFFIX' tag"
     echo "    -e, --repo  + \"repo_name\"          Build/run the image with the passed repository reference"
     echo "    -r, --ros_distro + \"distro_name\"   Build/run the image with the passed distro (the passed value will be also used to compose the image tag)"
-    echo "    -g, --gz_version + \"gz_version\"    Build/run the image with the passed gazebo version (the passed value will be also used to compose the image tag)"
     echo "    -y, --yarp_branch + \"yarp branch\"  Build/run the image with the passed yarp branch (the passed value, if different from \"master\", will be also used to compose the image tag)."
     echo "                                       If not passed, the branch used will be \"$YARP_DEF\""
     echo "        --inner_cycl_dds               If passed, the cyclone dds configuration file in app/navigation2/conf will be used, otherwise the one in ~/.config will be used."
@@ -92,16 +91,6 @@ get_opts()
                 shift
                 ROS_DISTRO=$1
                 ROS_SET=true
-                shift
-                ;;
-            -g|--gz_version)
-                if [[ $GZ_SET == "true" ]]; then
-                    echo "Gazebo version already set"
-                    usage
-                fi
-                shift
-                GZ_VERS=$1
-                GZ_SET=true
                 shift
                 ;;
             -y|--yarp_vers)
@@ -190,7 +179,6 @@ JUST_PRINT=false
 BUILD_SUFFIX=$DEVEL_SUFFIX
 VERSION="1.0.0"
 ROS_DISTRO=$ROS_DEF
-GZ_VERS=$GAZEBO_DEF
 YARP_BRANCH=$YARP_DEF
 PARENT_SUFFIX=$UBUNTU_SUFFIX
 IMAGE_SET=false
@@ -198,7 +186,6 @@ BUILD_SET=false
 GONNA_BUILD=false
 RUN_WITH_GPU=true
 ROS_SET=false
-GZ_SET=false
 YARP_SET=false
 IMAGE=$UBUNTU_DEF
 REPO=$REPO_DEF
@@ -220,7 +207,7 @@ COMPLETE_IMAGE_NAME=$REPO$REPO_SEP$BASE_TAG$JUNCTION$PARENT_SUFFIX$JUNCTION$ROS_
 
 if [[ $JUST_PRINT == "true" ]]; then
     if [[ $GONNA_BUILD == "true" ]]; then
-        echo "sudo docker build --build-arg base_img=$IMAGE --build-arg gazebo_version $GZ_VERS --build-arg ros_distro=$ROS_DISTRO --build-arg yarp_branch=$YARP_BRANCH --build-arg ros2_dev_branch=$ROS2_DEV_BRANCH --build-arg ros2_dev_remote=$ROS2_DEV_REMOTE  -t $COMPLETE_IMAGE_NAME ."
+        echo "sudo docker build --build-arg base_img=$IMAGE --build-arg ros_distro=$ROS_DISTRO --build-arg yarp_branch=$YARP_BRANCH --build-arg ros2_dev_branch=$ROS2_DEV_BRANCH --build-arg ros2_dev_remote=$ROS2_DEV_REMOTE  -t $COMPLETE_IMAGE_NAME ."
     else
         if [[ $RUN_WITH_GPU == "true" ]]; then
             echo "sudo docker run --rm -it --privileged --network host --pid host -e NVIDIA_DRIVER_CAPABILITIES=all -e DISPLAY -e ROS_DOMAIN_ID=${ROS_DOMAIN_ID} -v /tmp/.X11-unix:/tmp/.X11-unix -e QT_X11_NO_MITSHM=1 --gpus all $COMPLETE_IMAGE_NAME"
@@ -234,7 +221,7 @@ if [[ $JUST_PRINT == "true" ]]; then
 fi
 
 if [[ $GONNA_BUILD == "true" ]]; then
-    sudo docker build --build-arg base_img=$IMAGE --build-arg gazebo_version $GZ_VERS --build-arg ros_distro=$ROS_DISTRO --build-arg yarp_branch=$YARP_BRANCH --build-arg ros2_dev_branch=$ROS2_DEV_BRANCH --build-arg ros2_dev_remote=$ROS2_DEV_REMOTE  -t $COMPLETE_IMAGE_NAME .
+    sudo docker build --build-arg base_img=$IMAGE --build-arg ros_distro=$ROS_DISTRO --build-arg yarp_branch=$YARP_BRANCH --build-arg ros2_dev_branch=$ROS2_DEV_BRANCH --build-arg ros2_dev_remote=$ROS2_DEV_REMOTE  -t $COMPLETE_IMAGE_NAME .
 else
     sudo xhost +
     if [[ $RUN_WITH_GPU == "true" ]]; then
