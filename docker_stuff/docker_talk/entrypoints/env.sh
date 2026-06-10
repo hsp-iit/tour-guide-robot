d1f48843b70790da3deaffad4474faef515c84bf
Ettore Landini <ettore.landini@iit.it>
2026-06-10 18:31:41 +0200

    Fixed missing voice profiles in Dockerfile and removed unnecessary entrypoints and envs

--- docker_stuff/docker_talk/entrypoints/env.sh
+++
@@ -1,7 +0,0 @@
-export R1_OBR_REMOTE=origin
-export R1_OBR_BRANCH=master
-export TOUR_REMOTE=origin
-export TOUR_BRANCH=jazzy
-export ENTRY_POINT=/home/yarp-user/config/.entrypoint_light.sh
-
-xhost + local:docker
--- docker_stuff/docker_talk/entrypoints/env_e.sh
+++
@@ -1,3 +0,0 @@
-export ENTRY_POINT=/home/yarp-user/config/.entrypoint_empty.sh
-
-xhost + local:docker
