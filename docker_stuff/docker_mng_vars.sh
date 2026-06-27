17c38a132629ddee88369f673de624aca997c4cd
Ettore Landini <ettore.landini@iit.it>
2026-06-27 11:40:38 +0200

    Docker images almost done

--- docker_stuff/docker_mng_vars.sh
+++ docker_stuff/docker_mng_vars.sh
@@ -12,5 +12,5 @@ REPO_DEF="ghcr.io/hsp-iit/r1images"
 JUNCTION="_"
 REPO_SEP=":"
 ROS_DOMAIN_ID=37 #The number should be between 0-101 (https://docs.ros.org/en/iron/Concepts/Intermediate/About-Domain-ID.html#overview)
-ROS2_DEV_BRANCH="master"
+ROS2_DEV_BRANCH="YARP4"
 ROS2_DEV_REMOTE="robotology"
