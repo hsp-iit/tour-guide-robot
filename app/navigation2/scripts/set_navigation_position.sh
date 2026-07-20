fac2212a15aa9be32cd2c83b0030af61ab7f837b
leonardo gori <leonardo.gori@iit.it>
2026-07-20 11:59:12 +0200

    updated set navigation position to yarp 4
    Signed-off-by: leonardo gori <leonardo.gori@iit.it>

--- app/navigation2/scripts/set_navigation_position.sh
+++ app/navigation2/scripts/set_navigation_position.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
-echo "set pos 0 16.0511" | yarp write ... /cer/right_arm/rpc:i
-echo "set pos 1 9.1" | yarp write ... /cer/right_arm/rpc:i
-echo "set pos 2 14.2878" | yarp write ... /cer/right_arm/rpc:i
-echo "set pos 3 61.5511" | yarp write ... /cer/right_arm/rpc:i
-echo "set pos 4 0.0109864" | yarp write ... /cer/right_arm/rpc:i
-echo "set pos 5 0.0329591" | yarp write ... /cer/right_arm/rpc:i
-echo "set pos 6 0.0" | yarp write ... /cer/right_arm/rpc:i
-echo "set pos 7 0.0" | yarp write ... /cer/right_arm/rpc:i
 
-echo "set pos 0 16.0511" | yarp write ... /cer/left_arm/rpc:i
-echo "set pos 1 9.1" | yarp write ... /cer/left_arm/rpc:i
-echo "set pos 2 14.2878" | yarp write ... /cer/left_arm/rpc:i
-echo "set pos 3 61.5511" | yarp write ... /cer/left_arm/rpc:i
-echo "set pos 4 0.0109864" | yarp write ... /cer/left_arm/rpc:i
-echo "set pos 5 0.0329591" | yarp write ... /cer/left_arm/rpc:i
-echo "set pos 6 0.0" | yarp write ... /cer/left_arm/rpc:i
-echo "set pos 7 0.0" | yarp write ... /cer/left_arm/rpc:i
+echo "positionMoveGroupRPC (0 1 2 3 4 5 6 7) (16.0 9.0 14.0 61.0 0.01 0.03 0.0 0.0)" | yarp write ... /cer/right_arm/nws/rpc:i
 
-echo "set pos 0 0.012" | yarp write ... /cer/torso/rpc:i
+echo "positionMoveGroupRPC (0 1 2 3 4 5 6 7) (16.0 9.0 14.0 61.0 0.01 0.03 0.0 0.0)" | yarp write ... /cer/left_arm/nws/rpc:i
+
+echo "positionMoveOneRPC 0 0.012" | yarp write ... /cer/torso/nws/rpc:i
