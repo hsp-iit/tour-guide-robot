61ac0a5734c0a71d67db441fea9a2a0e96157eb1
Ettore Landini <ettore.landini@iit.it>
2026-07-28 15:14:39 +0200

    Added missing connection for laser repeater and updated set nav pos sim

--- app/navigation2/scripts/set_navigation_position_r1mk3Sim.sh
+++ app/navigation2/scripts/set_navigation_position_r1mk3Sim.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "positionMoveGroupRPC (0 1 2 3 4 5 6 7) (16.0511 9.1 14.2878 61.5511 0.0109864 0.0329591 0.0 0.0)" | yarp write ... /cer/right_arm/nws/rpc:i
+echo "positionMoveGroupRPC (0 1 2 3 4 5 6 7) (16.0511 9.1 14.2878 61.5511 0.0109864 0.0329591 0.0 0.0)" | yarp write ... /r1mk3Sim/right_arm/nws/rpc:i
 
-echo "positionMoveGroupRPC (0 1 2 3 4 5 6 7) (16.0511 9.1 14.2878 61.5511 0.0109864 0.03 0.0 0.0)" | yarp write ... /cer/left_arm/nws/rpc:i
+echo "positionMoveGroupRPC (0 1 2 3 4 5 6 7) (16.0511 9.1 14.2878 61.5511 0.0109864 0.03 0.0 0.0)" | yarp write ... /r1mk3Sim/left_arm/nws/rpc:i
 
-echo "positionMoveOneRPC 0 0.012" | yarp write ... /cer/torso/nws/rpc:i
+echo "positionMoveOneRPC 0 0.012" | yarp write ... /r1mk3Sim/torso/nws/rpc:i
