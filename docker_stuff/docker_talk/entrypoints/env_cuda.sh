44bd3d07b646b392b9010de5babff95a9405dde0
Ettore Landini <ettore.landini@iit.it>
2026-08-03 10:33:45 +0200

    Updated docker Talk

--- docker_stuff/docker_talk/entrypoints/env_cuda.sh
+++ docker_stuff/docker_talk/entrypoints/env_cuda.sh
@@ -1,7 +1,7 @@
 export R1_OBR_REMOTE=origin
 export R1_OBR_BRANCH=master
 export TOUR_REMOTE=origin
-export TOUR_BRANCH=upgrade/yarp-4
+export TOUR_BRANCH=jazzy
 export ENTRY_POINT=/home/yarp-user/config/.entrypoint_cuda.sh
 export AI_MODEL_PATH="${AI_MODEL_PATH:-$HOME/models}"
 
